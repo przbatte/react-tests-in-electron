cfa19434fbedfe9d00cf39ca67d27484d1b7fd36
Przemysław Batte <przemek@batte.pl>
2020-03-04 20:49:14 +0100

    inject css styles

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -1,14 +1,28 @@
 'use strict';
 
+const crypto = require('crypto')
+
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process() {
+  process(css, filename) {
+    const contentHash = crypto.createHash('sha256').update(css, 'utf8').digest('hex')
+
+    if (process.env.npm_package_jest_testEnvironment === 'jest-electron/environment') {
+      if (document.getElementById(contentHash) === null) {
+        const head = document.head || document.getElementsByTagName('head')[0]
+        const style = document.createElement('style')
+        style.appendChild(document.createTextNode(css))
+        style.type = 'text/css'
+        style.id = contentHash
+        head.appendChild(style)
+      }
+    }
+
     return 'module.exports = {};';
   },
-  getCacheKey() {
-    // The output is always the same.
-    return 'cssTransform';
+  getCacheKey(css) {
+    return Math.random().toString()
   },
 };
--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,11 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  debugger
   expect(linkElement).toBeInTheDocument();
+});
+
+test('does not render abc text', () => {
+  const { queryByText } = render(<App />);
+  const linkElement = queryByText(/abc/i);
+  expect(linkElement).not.toBeInTheDocument()
 });
